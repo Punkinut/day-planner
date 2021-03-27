e9520c40676f80fa242731c14e861eb963feccb6
Punkinut <heypunkinut@gmail.com>
2021-03-27 15:10:44 +1100

    Did basic grid layout for time table

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
+let anchorList = document.querySelectorAll(".scroll"); 
 // Script that shows the date on the main page
 $(".date").text(moment().format("DD[ /] MM[ /] YY"))
 
 // Script that shows the time on the main page
 $(".time").text(moment().format("h[ : ] mm A"))
+
+// Smooth scroll on buttons
+anchorList.forEach (function(link) {
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+        let destination =  document.querySelector(this.hash); 
+        destination.scrollIntoView({behavior: "smooth"});
+    });
+})
