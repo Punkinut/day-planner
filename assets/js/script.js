c75a61fe4ea111f53f41aa1c35897e8d9dab45be
Punkinut <heypunkinut@gmail.com>
2021-03-28 11:12:49 +1100

    Added the color coded dots in each grid that change depending on the time of the day

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,79 @@ anchorList.forEach (function(link) {
     });
 })
 
+// Gather the hour of the day
+var hour = moment().format("k")
+// Convert the hour to an interger
+var hourToInt = parseInt(hour);
+
+// Determines the circle color on the hour of the day in 9
+if(hourToInt === 9) {
+    $(".nine-red").css('display', 'block')
+} else if (hourToInt > 9) {
+    $(".nine-gray").css('display', 'block')
+} else {
+    $(".nine-green").css('display', 'block')
+}
+
+// Determines the circle color on the hour of the day in 10
+if(hourToInt === 10) {
+    $(".ten-red").css('display', 'block')
+} else if (hourToInt > 10) {
+    $(".ten-gray").css('display', 'block')
+} else {
+    $(".ten-green").css('display', 'block')
+}
+
+// Determines the circle color on the hour of the day in 11
+if(hourToInt === 11) {
+    $(".eleven-red").css('display', 'block')
+} else if (hourToInt > 11) {
+    $(".eleven-gray").css('display', 'block')
+} else {
+    $(".eleven-green").css('display', 'block')
+}
+
+// Determines the circle color on the hour of the day in 12
+if(hourToInt === 12) {
+    $(".twelve-red").css('display', 'block')
+} else if (hourToInt > 12) {
+    $(".twelve-gray").css('display', 'block')
+} else {
+    $(".twelve-green").css('display', 'block')
+}
+
+// Determines the circle color on the hour of the day in 1
+if(hourToInt === 13) {
+    $(".one-red").css('display', 'block')
+} else if (hourToInt > 13) {
+    $(".one-gray").css('display', 'block')
+} else {
+    $(".one-green").css('display', 'block')
+}
+
+// Determines the circle color on the hour of the day in 2
+if(hourToInt === 14) {
+    $(".two-red").css('display', 'block')
+} else if (hourToInt > 14) {
+    $(".two-gray").css('display', 'block')
+} else {
+    $(".two-green").css('display', 'block')
+}
+
+// Determines the circle color on the hour of the day in 3
+if(hourToInt === 15) {
+    $(".three-red").css('display', 'block')
+} else if (hourToInt > 15) {
+    $(".three-gray").css('display', 'block')
+} else {
+    $(".three-green").css('display', 'block')
+}
+
+// Determines the circle color on the hour of the day in 4
+if(hourToInt === 16) {
+    $(".four-red").css('display', 'block')
+} else if (hourToInt > 16) {
+    $(".four-gray").css('display', 'block')
+} else {
+    $(".four-green").css('display', 'block')
+}
